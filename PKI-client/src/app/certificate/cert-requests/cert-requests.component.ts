40104514be6a93bb5e00db5b265ee5694056074a
Dusan Djordjevic <djordjevic.sv1.2021@uns.ac.rs>
2024-04-20 17:18:20 +0200

    feat: Add navbar

---
+++ PKI-client/src/app/certificate/cert-requests/cert-requests.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cert-requests',
+  templateUrl: './cert-requests.component.html',
+  styleUrls: ['./cert-requests.component.css']
+})
+export class CertRequestsComponent {
+
+}
--- PKI-client/src/app/layout/layout.module.ts
+++ PKI-client/src/app/layout/layout.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from '../infrastructure/material/material.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [
+    NavbarComponent
   ]
 })
 export class LayoutModule { }
