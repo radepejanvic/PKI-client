f1e8596341170eff48dbb61595efd49c8fc9a7ca
Dusan Djordjevic <djordjevic.sv1.2021@uns.ac.rs>
2024-04-20 16:53:35 +0200

    feat: add auth and login

--- PKI-client/src/app/app.module.ts
+++ PKI-client/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {Interceptor} from "./infrastructure/auth/interceptor";
+import { AuthModule } from './infrastructure/auth/auth.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: Interceptor,
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
